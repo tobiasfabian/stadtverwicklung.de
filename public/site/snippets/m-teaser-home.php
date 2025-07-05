f8ab1f89b04e4edf6d6e086ef8630e0f7f7252a6
Tobias Wolf <hello@tobiaswolf.me>
2025-07-05 12:39:23 +0200

    Improve responsive design
    Signed-off-by: Tobias Wolf <hello@tobiaswolf.me>

--- public/site/snippets/m-teaser-home.php
+++ public/site/snippets/m-teaser-home.php
@@ -8,20 +8,34 @@ if ($teaserImage === null) {
 }
 ?>
 <div class="m-teaser-home">
-	<?php if ($teaserImage = $page->teaserImage()->toFile()): ?>
-		<?php snippet('image', [
-			'image' => $teaserImage,
-			'sizes' => '100vw',
-		]) ?>
-	<?php endif ?>
-	<div class="m-teaser-home__text">
-		<h1 class="a-heading"><?= $page->teaserTitle() ?></h1>
-		<p class="m-text"><?= $page->teaserText() ?></p>
-		<?php if ($page->teaserLink()->isNotEmpty()): ?>
-			<a class="a-button" data-kind="transparent" href="<?= $teaserLink->link()->toUrl() ?>">
-				<?= $teaserLink->text() ?>
-			</a>
+	<div>
+		<?php if ($teaserImage = $page->teaserImage()->toFile()): ?>
+			<picture>
+				<source srcset="<?= $teaserImage->thumb([
+					'height' => 360,
+					'width' => 800,
+					'crop' => true,
+				])->url() ?> 1x, <?= $teaserImage->thumb([
+					'height' => 360 * 2,
+					'width' => 800 * 2,
+					'quality' => 60,
+					'crop' => true,
+				])->url() ?> 2x" media="(max-width: 50em)">
+				<?php snippet('image', [
+					'image' => $teaserImage,
+					'sizes' => '100vw',
+				]) ?>
+			</picture>
 		<?php endif ?>
+		<div class="m-teaser-home__text">
+			<h1 class="a-heading"><?= $page->teaserTitle() ?></h1>
+			<p class="m-text"><?= $page->teaserText() ?></p>
+			<?php if ($page->teaserLink()->isNotEmpty()): ?>
+				<a class="a-button" data-kind="transparent" href="<?= $teaserLink->link()->toUrl() ?>">
+					<?= $teaserLink->text() ?>
+				</a>
+			<?php endif ?>
+		</div>
 	</div>
 </div>
 
