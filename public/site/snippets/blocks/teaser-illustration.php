754bd62cf15188a81fad9d568aae3af5da4f7820
Tobias Wolf <hello@tobiaswolf.me>
2025-12-19 11:13:40 +0100

    Add m-teaser-illustration
    Remove standard teaser from active projects
    
    Signed-off-by: Tobias Wolf <hello@tobiaswolf.me>

---
+++ public/site/snippets/blocks/teaser-illustration.php
@@ -0,0 +1,43 @@
+<?php /** @var \Kirby\Cms\Block $block */ ?>
+<?php
+$image = $block->image()->toFile();
+$link = $block->link()->toObject();
+$headline = $block->headline();
+$text = $block->text();
+
+if ($image === null) {
+	return;
+}
+?>
+<div class=m-teaser-illustration>
+	<div>
+		<?php if ($image): ?>
+			<picture>
+				<source srcset="<?= $image->thumb([
+					'height' => 360,
+					'width' => 800,
+					'crop' => true,
+				])->url() ?> 1x, <?= $image->thumb([
+					'height' => 360 * 2,
+					'width' => 800 * 2,
+					'quality' => 60,
+					'crop' => true,
+				])->url() ?> 2x" media="(max-width: 50em)">
+				<?php snippet('image', [
+					'image' => $image,
+					'sizes' => '100vw',
+				]) ?>
+			</picture>
+		<?php endif ?>
+		<div class="m-teaser-illustration__text">
+			<h1 class="a-heading"><?= $headline ?></h1>
+			<p class="m-text"><?= $text ?></p>
+			<?php if ($link->link()->toUrl()): ?>
+				<a class="a-button" data-kind="transparent" href="<?= $link->link()->toUrl() ?>">
+					<?= $link->text() ?>
+				</a>
+			<?php endif ?>
+		</div>
+	</div>
+</div>
+
--- public/site/templates/project.php
+++ public/site/templates/project.php
@@ -6,35 +6,35 @@
 	<?php snippet('o-header') ?>
 	<main>
 		<div class=o-blocks>
-			<div class=m-teaser>
-				<h1 class=a-heading><?= $page->alternativeTitle()->or($page->title()) ?></h1>
-				<div class=m-teaser__text>
-					<?php if ($page->teaserText()->isNotEmpty()): ?>
-						<div class=m-text>
-							<?= $page->teaserText() ?>
-						</div>
-					<?php endif ?>
-					<?php if ($page->status() === 'unlisted'): ?>
-						<div class=m-text>
-							<dl>
-								<dt>Zeitraum</dt>
-								<dd><?= $page->period() ?></dd>
-								<dt>Ort</dt>
-								<dd><?= $page->location() ?></dd>
-								<dt>Handlungsfelder</dt>
-								<dd><?= $page->areas_of_action() ?></dd>
-							</dl>
-							<p><strong><?= $page->partner()->kti()?></strong></p>
-						</div>
+			<?php if ($page->status() === 'unlisted'): ?>
+				<div class=m-teaser>
+					<h1 class=a-heading><?= $page->alternativeTitle()->or($page->title()) ?></h1>
+					<div class=m-teaser__text>
+						<?php if ($page->teaserText()->isNotEmpty()): ?>
+							<div class=m-text>
+								<?= $page->teaserText() ?>
+							</div>
+						<?php endif ?>
+							<div class=m-text>
+								<dl>
+									<dt>Zeitraum</dt>
+									<dd><?= $page->period() ?></dd>
+									<dt>Ort</dt>
+									<dd><?= $page->location() ?></dd>
+									<dt>Handlungsfelder</dt>
+									<dd><?= $page->areas_of_action() ?></dd>
+								</dl>
+								<p><strong><?= $page->partner()->kti()?></strong></p>
+							</div>
+					</div>
+					<?php if ($teaserImage = $page->teaserImage()->toFile()): ?>
+						<?php snippet('image', [
+							'image' => $teaserImage,
+							'sizes' => 528 / 16 . 'em',
+						]) ?>
 					<?php endif ?>
 				</div>
-				<?php if ($teaserImage = $page->teaserImage()->toFile()): ?>
-					<?php snippet('image', [
-						'image' => $teaserImage,
-						'sizes' => 528 / 16 . 'em',
-					]) ?>
-				<?php endif ?>
-			</div>
+			<?php endif ?>
 
 			<?= $page->text()->toBlocks() ?>
 		</div>
