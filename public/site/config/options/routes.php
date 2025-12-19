e25ceac05a1e3473c9676494ee6482a33ed3a131
Tobias Wolf <hello@tobiaswolf.me>
2025-12-19 18:01:43 +0100

    Add redirects
    Signed-off-by: Tobias Wolf <hello@tobiaswolf.me>

--- public/site/config/options/routes.php
+++ public/site/config/options/routes.php
@@ -2,6 +2,54 @@
 
 return [
 	[
+		'pattern' => 'media/pages/programme',
+		'action' => function($all) {
+			go('media/pages/projekte/' . $all, 301);
+		}
+	],
+	[
+		'pattern' => 'de(:all)',
+		'action' => function($all) {
+			go($all, 301);
+		}
+	],
+	[
+		'pattern' => 'programme(:all)',
+		'action' => function($all) {
+			go('projekte' . $all, 301);
+		}
+	],
+	[
+		'pattern' => 'uber-uns(:all)',
+		'action' => function($all) {
+			go('ueber-uns' . $all, 301);
+		}
+	],
+	[
+		'pattern' => 'en/uber-uns(:all)',
+		'action' => function($all) {
+			go('en/ueber-uns' . $all, 301);
+		}
+	],
+	[
+		'pattern' => 'leistungen/fur-kommunen',
+		'action' => function() {
+			go('leistungen/fuer-kommunen', 301);
+		}
+	],
+	[
+		'pattern' => 'leistungen/fur-initiativen',
+		'action' => function() {
+			go('leistungen/fuer-initiativen', 301);
+		}
+	],
+	[
+		'pattern' => 'leistungen/fur-private-tragerschaften',
+		'action' => function() {
+			go('leistungen/fuer-private-traegerschaften', 301);
+		}
+	],
+	[
 		'pattern' => 'site.webmanifest',
 		'action' => function() {
 			$content = snippet('manifest', ['site' => site()], true);
